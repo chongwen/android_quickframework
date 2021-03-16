e8d8ca1f5921c8faecd8ff90fdcb27a87b6bda3c
KC <zhongchongwen@gmail.com>
2021-03-16 22:23:11 +0800

    代码格式化

--- app/src/main/java/com/example/quickapplication/FragmentTabHostActivity.java
+++ app/src/main/java/com/example/quickapplication/FragmentTabHostActivity.java
@@ -20,9 +20,10 @@ import static com.example.quickapplication.DataGenerator.mTabTitle;
  * Created by zhouwei on 17/4/23.
  */
 
-public class FragmentTabHostActivity extends AppCompatActivity implements TabHost.OnTabChangeListener{
-    private Fragment[]mFragments;
+public class FragmentTabHostActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
+    private Fragment[] mFragments;
     private FragmentTabHost mTabHost;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,23 +33,23 @@ public class FragmentTabHostActivity extends AppCompatActivity implements TabHos
 
     }
 
-    private void initView(){
+    private void initView() {
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
 
         // 关联TabHost
-        mTabHost.setup(this,getSupportFragmentManager(),R.id.home_container);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.home_container);
         //注意，监听要设置在添加Tab之前
         mTabHost.setOnTabChangedListener(this);
 
 
         //添加Tab
-        for (int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             //生成TabSpec
-            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTabTitle[i]).setIndicator(DataGenerator.getTabView(this,i));
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTabTitle[i]).setIndicator(DataGenerator.getTabView(this, i));
             // 添加Tab 到TabHost，并绑定Fragment
             Bundle bundle = new Bundle();
-            bundle.putString("from","FragmentTabHost Tab");
-            mTabHost.addTab(tabSpec,mFragments[i].getClass(),bundle);
+            bundle.putString("from", "FragmentTabHost Tab");
+            mTabHost.addTab(tabSpec, mFragments[i].getClass(), bundle);
         }
 
 
@@ -59,26 +60,25 @@ public class FragmentTabHostActivity extends AppCompatActivity implements TabHos
     }
 
 
-
     @Override
     public void onTabChanged(String tabId) {
-       updateTabState();
+        updateTabState();
 
     }
 
     /**
      * 更新Tab 的状态
      */
-    private void updateTabState(){
+    private void updateTabState() {
         TabWidget tabWidget = mTabHost.getTabWidget();
-        for (int i=0;i<tabWidget.getTabCount();i++){
+        for (int i = 0; i < tabWidget.getTabCount(); i++) {
             View view = tabWidget.getChildTabViewAt(i);
             ImageView tabIcon = (ImageView) view.findViewById(R.id.tab_content_image);
-            TextView  tabText = (TextView) view.findViewById(R.id.tab_content_text);
-            if(i == mTabHost.getCurrentTab()){
+            TextView tabText = (TextView) view.findViewById(R.id.tab_content_text);
+            if (i == mTabHost.getCurrentTab()) {
                 tabIcon.setImageResource(DataGenerator.mTabResPressed[i]);
                 tabText.setTextColor(getResources().getColor(android.R.color.black));
-            }else{
+            } else {
                 tabIcon.setImageResource(mTabRes[i]);
                 tabText.setTextColor(getResources().getColor(android.R.color.darker_gray));
             }
